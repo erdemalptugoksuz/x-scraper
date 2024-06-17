1a70dc7cab732dbabef316a6a4d4df83487040a8
Erdem Alptuğ Öksüz <erdemalpoksuz@gmail.com>
2024-06-18 00:18:40 +0300

    dev/X-101 Get user info modules completed.

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-print("Hello world!")
+from services.get_user_top_following_accounts import get_user_top_following_accounts
+from services.get_user_medias import get_user_medias
+
+""" local variables """
+user_ids = ["17368259", "1681595742018756608"]
+
+
+def main():
+    for user_id in user_ids:
+        all_user_medias = get_user_medias(user_id)
+        print(all_user_medias)
+
+        all_following_accounts = get_user_top_following_accounts(user_id)
+        print(all_following_accounts)
+
+
+if __name__ == "__main__":
+    main()
